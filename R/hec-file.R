f1f520f8cb97dab2759735314124e59fa3c685bd
ergz <e.emanuel.rodriguez@gmail.com>
2017-07-07 00:00:47 -0700

    on load function implemented

--- R/hec-file.R
+++ R/hec-file.R
@@ -4,19 +4,3 @@
 hec_file <- function(filename) {
   h5::h5file(filename)
 }
-
-#' Function retrieves top level metadata associated with a hec ras file
-#' @param hf a \code{hec_file} object 
-#' @return list of attributes associated with hecras output file
-#' @export
-hec_attributes <- function(hf) {
-  file_type <- h5::h5attr(hf, 'File Type')
-  file_version <- h5::h5attr(hf, 'File Version')
-  file_projection <- h5::h5attr(hf, 'Projection')
-  file_units <- h5::h5attr(hf, 'Units System')
-  
-  list('file_type' = file_type, 
-       'file_version' = file_version, 
-       'file_projection' = file_projection, 
-       'file_units' = file_units)
-}
