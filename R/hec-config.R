3aad76895d5fa863135517d5ea3c85acc164907a
Emanuel Rodriguez <erodriguez@flowwest.com>
2017-09-12 17:10:29 -0700

    started working on a config file based workflow

--- R/hec-config.R
+++ R/hec-config.R
@@ -1,5 +1,11 @@
 # Implement the use of config files for creating queries 
 
+new_config <- function(name, template = 'default') {
+  wd <- getwd()
+  filename <- paste0(wd, "/", name, ".yml")
+  file.create(filename)
+}
+
 #' Function reads in and fills in a query based on a configuration file parameter
 #' @param config_file path to config file used to fill query
 #' @param type a defined config type in the config file. See README for more information 
@@ -9,4 +15,10 @@ hec_config <- function(config_file, type = 'default') {
   cfg <- config::get(config = type)
   
   return(cfg)
+}
+
+
+
+run_confg <- function(config) {
+  
 }
