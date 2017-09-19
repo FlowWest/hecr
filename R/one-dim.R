06469499cbef18224ec11f910f5afed04b5d216e
ergz <e.emanuel.rodriguez@gmail.com>
2017-09-19 12:44:32 -0700

    added timestamp query to ts1
    this commit adds the first iteration of the timestamp query.
    This feature allows users to query out a single timestamp
    from the model run. Doing so allows them to query line profile, it
    also saves A LOT of computation time when user is querying from large
    collection of hdf files.

--- R/one-dim.R
+++ R/one-dim.R
@@ -41,31 +41,35 @@ get_xs_river_name <- function(.f, station_idx) {
 #' @param f an hdf5 file read in via hec_file or a corpus created with create_hdf_corpus
 #' @param station_name station for the cross section to query time series from 
 #' @param ts_type time series to query out (ex 'Water Surface', 'Depth', ...)
-#' @param ... additional query options specified by name. (e.g timestamp ="2006-01-01 18:15:00")
+#' @param timestatmp an optional timestamp to query, default action will not filter for a timestamp
 #' @export
-extract_ts1 <- function(f, station_name, ts_type="Water Surface") {
+extract_ts1 <- function(f, station_name, ts_type="Water Surface", timestamp=NULL) {
   # closure for extracting each file in f
-  do_extract <- function(.f, station_name, ts, timestamp=timestamp) {
+  
+  do_extract <- function(.f, station_name, ts, timestamp) {
     xs_datetime <- get_model_timestamps(.f)
+    # if user supplied timestamp argument then find its index else return all indexes
+    # TODO: case when timestamp was not found needs to have stop(ERROR)
+    timestamp_idx <- if(!is.null(timestamp)) which(xs_datetime == timestamp) else seq_len(length(xs_datetime))
     plan_id <- get_plan_attributes(.f)$plan_short_id
     xs_index <- get_xs_station_index(.f, station_name)
     river_name <- get_xs_river_name(.f, xs_index)
     reach_name <- get_xs_reach(.f, xs_index)
-    d_length <- length(xs_datetime)
-    series <-matrix(.f[hdf_paths$RES_CROSS_SECTIONS][ts_type][, xs_index], 
+    d_length <- length(timestamp_idx)
+    series <-matrix(.f[hdf_paths$RES_CROSS_SECTIONS][ts_type][timestamp_idx, xs_index], 
              ncol=1, byrow=FALSE)
     
-      
+          
     
-    tibble:::tibble("datetime" = rep(xs_datetime, length(xs_index)),
-                    "plan_id" = plan_id,
+    tibble:::tibble("datetime" = rep(xs_datetime[timestamp_idx], length(xs_index)),
+                    "plan_id" = plan_id,# reps on its own
                     "river_name" = rep(river_name, each=d_length), 
                     "reach_name" = rep(reach_name, each=d_length), 
                     "cross_section" = rep(station_name, each=d_length), 
                     "values" = series[,1])  
   }
   
-  x <- purrr::map_dfr(f, ~do_extract(., station_name, ts_type))
+  x <- purrr::map_dfr(f, ~do_extract(., station_name, ts_type, timestamp))
   attr(x, 'hec_obj') <- f
   x
 }
