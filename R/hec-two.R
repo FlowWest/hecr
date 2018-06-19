a4e6aba47759727ba41a19f6d6a8e026efa016d6
ergz <erodriguez@flowwest.com>
2018-06-19 14:22:18 -0700

    patched up the hec_two function to work with new design, will need to refactor tho

--- R/hec-two.R
+++ R/hec-two.R
@@ -20,67 +20,51 @@
 #' ws <- extract_ts2(f, xy=c(4567654.0, 2167453.0), "Water Surface", timestamp="2005-09-12 00:00:00")
 #' }
 #' @export
-hec_two <- function(f, xy, ts_type = "Water Surface", time_stamp = NULL) {
+hec_two <- function(hc, xy, ts_type = "Water Surface", time_stamp = NULL) {
   
-  do_extract <- function(.f) {
-    
-    timestamps <- hec_timestamps_(.f)
-    attrs <- hec_info_(.f) 
-    area_name <- hec_flow_area_(.f)
-    model_center_coordinates <- hec_center_coords_(.f, area_name)
-    
-    if (!is.null(time_stamp)) {
-      time_idx <- which(timestamps == time_stamp)
-      if (length(time_idx) == 0) stop("supplied value for time_stamp was not found in the model", 
-                                      call. = FALSE)
-    } else {
-      time_idx <- seq_len(length(timestamps))
-    }
-    
-    input_coordinates <- make_coordinate_matrix(xy)
-    
-    nearest_cell_index <- sapply(seq_len(nrow(input_coordinates)), function(i) {
-      get_nearest_cell_center_index(input_coordinates[i,], model_center_coordinates)
-    }) 
-    
-    nearest_cell_index_values <- sort(unique(unlist(nearest_cell_index)))
-    
-    # Warn the user when some coordinates provided were within the same cell
-    if (length(nearest_cell_index) != length(nearest_cell_index_values)) {
-      warning("some of the coordinates provided were mapped to the same cell", 
-              call. = FALSE)
-    }
-    
-    time_series <- .f[[hdf_paths$RES_2D_FLOW_AREAS]][[area_name]][[ts_type]][nearest_cell_index_values, time_idx]
-    stacked_time_series <- matrix(t(time_series), ncol=1, byrow = TRUE)
-    
-    hdf_cell_index <- nearest_cell_index_values - 1
-    
-    tibble::tibble(
-      "datetime" = rep(timestamps[time_idx], length(nearest_cell_index_values)), 
-      "plan_id" = attrs$plan_short_id,
-      "plan_file" = attrs$plan_file,
-      "time_series_type" = ts_type, 
-      "hdf_cell_index" = rep(hdf_cell_index, each = length(time_idx)), 
-      "values" = as.vector(stacked_time_series)
-    )
+  timestamps <- hec_timestamps_(hc)
+  attrs <- hec_info(hc) 
+  area_name <- hec_flow_area_(hc)
+  model_center_coordinates <- hec_center_coords_(hc, area_name)
+  
+  if (!is.null(time_stamp)) {
+    time_idx <- which(timestamps == time_stamp)
+    if (length(time_idx) == 0) stop("supplied value for time_stamp was not found in the model", 
+                                    call. = FALSE)
+  } else {
+    time_idx <- seq_len(length(timestamps))
   }
   
-  purrr::map_dfr(f$collection, ~do_extract(.))
+  input_coordinates <- make_coordinate_matrix(xy)
   
+  nearest_cell_index <- sapply(seq_len(nrow(input_coordinates)), function(i) {
+    get_nearest_cell_center_index(input_coordinates[i,], model_center_coordinates)
+  }) 
+  
+  nearest_cell_index_values <- sort(unique(unlist(nearest_cell_index)))
+  
+  # Warn the user when some coordinates provided were within the same cell
+  if (length(nearest_cell_index) != length(nearest_cell_index_values)) {
+    warning("some of the coordinates provided were mapped to the same cell", 
+            call. = FALSE)
+  }
+  
+  time_series <- hc$object[[hdf_paths$RES_2D_FLOW_AREAS]][[area_name]][[ts_type]][nearest_cell_index_values, time_idx]
+  stacked_time_series <- matrix(t(time_series), ncol=1, byrow = TRUE)
+  
+  hdf_cell_index <- nearest_cell_index_values - 1
+  
+  tibble::tibble(
+    "datetime" = rep(timestamps[time_idx], length(nearest_cell_index_values)), 
+    "plan_id" = attrs$plan_short_id,
+    "plan_file" = attrs$plan_file,
+    "time_series_type" = ts_type, 
+    "hdf_cell_index" = rep(hdf_cell_index, each = length(time_idx)), 
+    "values" = as.vector(stacked_time_series)
+  )
+
 }
 
-#' Show center coordinates
-#' @description show the center coordinates for items in a collection
-#' @param hc a hec_collection object
-#' @export
-hec_coordinates <- function(hc) {
-  if (!is_hec_collection(hc)) {
-    stop("hc is not a hec_collection")
-  }
-  purrr::map(hc$collection, ~hec_center_coords_(., area_name = hec_flow_area_(.))) %>% 
-    purrr::set_names(hc$files)
-}
 
 # INTERNALS
 
@@ -89,11 +73,11 @@ hec_coordinates <- function(hc) {
 
 hec_flow_area_ <- function(f) {
   path_to_areas <- "Results/Unsteady/Output/Output Blocks/Base Output/Unsteady Time Series/2D Flow Areas"
-  names(f[[path_to_areas]])
+  names(f$object[[path_to_areas]])
 }
 
 hec_center_coords_ <- function(f, area_name) {
-  d <- f[[hdf_paths$GEOM_2D_AREAS]][[area_name]][["Cells Center Coordinate"]]
+  d <- f$object[[hdf_paths$GEOM_2D_AREAS]][[area_name]][["Cells Center Coordinate"]]
   on.exit(d$close())
   
   return(d[,])
