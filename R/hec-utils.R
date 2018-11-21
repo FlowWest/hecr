bed4306c6973aabf8d303325001f72c94774eaf5
emanuel <erodriguez@flowwest.com>
2018-11-21 11:20:28 -0800

    refactor the list datasets function to be more robust

--- R/hec-utils.R
+++ R/hec-utils.R
@@ -1,7 +1,7 @@
 #' List Available data
 #' @description list the data that is avaialble to query on. This values
 #' depends on whether a query is set to 'one' or 'two' dimensional domains
-#' @param hc a hec object
+#' @param hc a "hec" object 
 #' @param domain which domain to list data for, will default to both options are 'one' or 'two'
 #' @export 
 hec_datasets <- function(hc, domain=NULL) {
@@ -11,16 +11,25 @@ hec_datasets <- function(hc, domain=NULL) {
     area = get_plan_area(hc)
     )
   
-  cat(two_path, "\n")
   one <- NULL
   two <- NULL
   
   if (is.null(domain)) {
-    one <- hc$object[[one_path]]$ls()
-    two <- hc$object[[two_path]]$ls()
+    if (has_crossections(hc)) 
+      one <- hc$object[[one_path]]$ls()
+    if (has_2d(hc))
+      two <- hc$object[[two_path]]$ls()
+    
+    if (is.null(one) & is.null(two)) stop("Neither 'one' or'two' domains available for this model")
+    if (is.null(one)) warning("this model only has domain 'two' defined")
+    if (is.null(two)) warning("this model only has domain 'one' defined")
   } else if (domain == 'one') {
+    if (!has_crossections(hc)) stop("Domain request was 'one' but is not available for this model", 
+                                    call.=FALSE)
     one <- hc$object[[one_path]]$ls()
   } else if (domain == 'two') {
+    if (!has_2d(hc)) stop("Domain request was 'two' but not available for this model", 
+                          call. = FALSE)
     two <- hc$object[[two_path]]$ls()
   }
   structure(
@@ -48,6 +57,10 @@ print.hec_datasets <- function(x, ...) {
 
 has_crossections <- function(h) {
   "Cross Sections" %in% names(h$object[["Geometry"]])
+}
+
+has_2d <- function(h) {
+  "2D Flow Areas" %in% names(h$object[["Geometry"]])
 }
 
 hec_timestamps_ <- function(h) {
