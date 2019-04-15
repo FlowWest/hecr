9777760ee719cbc155cc6cfcc39194181a45a854
emanuel <erodriguez@flowwest.com>
2019-04-15 15:18:53 -0700

    update the docs site

--- R/hecr.R
+++ R/hecr.R
@@ -7,19 +7,3 @@
 #' @importFrom dplyr %>%
 #' 
 NULL
-
-#' Print list of datasets 
-#' @description given a hec_collection object passed through one of 'hec_one' or 'hec_two' 
-#' print the available data sets to query
-#' @export 
-hec_ls <- function(x, ...) {
-  UseMethod("hec_ls", x)
-}
-
-#' Extract a time series
-#' @description given a hec_collection object passed through one of 'hec_one' or 'hec_two'
-#' extract a time series
-#' @export
-hec_extract <- function(x, ...) {
-  UseMethod("hec_extract", x)
-}
--- R/utils.R
+++ R/utils.R
@@ -1,35 +1,3 @@
-#' Get model timestamps 
-#' This should correspond with the width of raw time series dataframes
-get_model_timestamps <- function(f) {
 
-  d <- f[[hdf_paths$RES_UNSTEADY_TS]][['Time Date Stamp']]
-  on.exit(d$close())
-  
-  as.POSIXct(trimws(d[]), format = "%d%b%Y %H:%M:%S")
-}
-
-#' Get model metadata
-get_model_metadata <- function(f) {
-  plan_info <- f[[hdf_paths$PLAN_INFORMATION]]
-  on.exit(plan_info$close())
-  
-  plan_file_lng_name = hdf5r::h5attr(plan_info, "Plan File")
-  plan_file <- stringr::str_extract(plan_file_lng_name, "[A-Za-z]+.p[0-9]+$")
-  structure(
-    list(
-      plan_file = plan_file,
-      plan_name = hdf5r::h5attr(plan_info, "Plan Name"), 
-      plan_id = hdf5r::h5attr(plan_info, "Plan ShortID"), 
-      geometry = hdf5r::h5attr(plan_info, "Geometry Title")
-    ), 
-    class = "hec_metadata"
-  )
-}
-
-
-
-is_hec_collection <- function(f) {
-  !is.atomic(f) & inherits(f, "hec_collection")
-}
 
 is_empty <- function(x) length(x) == 0L
